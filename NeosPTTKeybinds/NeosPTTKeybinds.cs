febd8ee2e6239be3a6473077eb4624183b22034d
Delta <delta@deltawolf.tk>
2022-08-01 08:52:10 -0700

    Config description changes

--- NeosPTTKeybinds/NeosPTTKeybinds.cs
+++ NeosPTTKeybinds/NeosPTTKeybinds.cs
@@ -12,19 +12,19 @@ namespace NeosPTTKeybinds {
 
 		//TODO keycode config for rebinding activation keys
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<bool> disableMuteKey = new ModConfigurationKey<bool>("disableMuteKey", "Disables the Mute toggle keybind M", () => true);
+		private static readonly ModConfigurationKey<bool> disableMuteKey = new ModConfigurationKey<bool>("disableMuteKey", "Disable the Mute toggle keybind M", () => true);
 
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<bool> disablePTTKeys = new ModConfigurationKey<bool>("disableMousePTTKey", "Disables the PTT keybind Mouse5 and V", () => true);
+		private static readonly ModConfigurationKey<bool> disablePTTKeys = new ModConfigurationKey<bool>("disableMousePTTKey", "Disable the PTT keybind Mouse5 and V", () => true);
 
 		[AutoRegisterConfigKey]
 		private static readonly ModConfigurationKey<bool> remapKeys = new ModConfigurationKey<bool>("remapKeys", "Enable to remap keys, will disable all default PTT keys", () => false);
 
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<Key> customMuteKey = new ModConfigurationKey<Key>("customMuteKey", "Set Custom Mute Keybind, set to 0 to disable", () => Key.M);
+		private static readonly ModConfigurationKey<Key> customMuteKey = new ModConfigurationKey<Key>("customMuteKey", "Set custom Mute keybind, set to 0 to disable", () => Key.M);
 
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<Key> customPTTKey = new ModConfigurationKey<Key>("customPTTKey", "Set Custom PTT Keybind, set to 0 to disable", () => Key.V);
+		private static readonly ModConfigurationKey<Key> customPTTKey = new ModConfigurationKey<Key>("customPTTKey", "Set custom PTT keybind, set to 0 to disable", () => Key.V);
 
 		private static ModConfiguration Config;
 
